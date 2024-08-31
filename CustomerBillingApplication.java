982d22a070d9be0c7debf9c5c8bae36c522ba3b6
mayeerhamidullah <150137265+mayeerhamidullah@users.noreply.github.com>
2024-08-31 16:48:30 +0530

    Add files via upload
    my spring boot project's base class.

---
+++ CustomerBillingApplication.java
@@ -0,0 +1,13 @@
+package com.BILLING;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerBillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerBillingApplication.class, args);
+	}
+
+}
